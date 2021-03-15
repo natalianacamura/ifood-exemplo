efbc40051724f179afb69f0cffff7f89ca3e9a10
Natalia Nacamura <natalia.nacamura@qikserve.com>
2021-03-15 20:21:48 -0300

    Começando projeto

---
+++ cadastro/src/test/java/com/ifoodexemplo/cadastro/CadastroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ifoodexemplo.cadastro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
